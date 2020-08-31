f96bd22cf769268cb4ef93007bbea8d309d8b497
sheila <sms493@cornell.edu>
2020-08-31 16:44:19 -0400

    fixes coop data grab for loop

--- ncso_api_retrieval_script.R
+++ ncso_api_retrieval_script.R
@@ -61,6 +61,7 @@
 
 # ---- to do ----
 
+# TODO fill out metadata column description in notes
 # TODO use here package for paths - hard coding them for now
 
 
@@ -84,17 +85,17 @@ source(paste0(functions_path, "read_ncsco_api_metadata.R"))
 NCSOC_API_KEY <- Sys.getenv("NCSOC_API_KEY")
 
 # data export path
-data_export_path <- "/Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/data/tabular/sheila_generated/sco_api_hist_data/"
+data_export_path <- "/Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/data/tabular/sco_api_raw/"
 
 
 # ---- 4. define dates of interest ----
 
-# date list df with start and end date for first step
-date_step_list <- data.frame(start_date = ymd("2016-01-01"),
-                             end_date = ymd("2016-01-31"))
+# date list df with start and end date for first step (i.e., the first month)
+date_step_list <- data.frame(start_date = ymd("2015-01-01"),
+                             end_date = ymd("2015-01-31"))
 
 # number of months
-num_months <- 12
+num_months <- 24 # for 2015 and 2016 (12 x 2 = 24)
 
 # loop
 for(i in 2:num_months) {
@@ -121,48 +122,89 @@ ncsco_api_datasets_sel <- "COOP"
 
 # iterate through date_step_list
 
+coop_data_raw <- data.frame(location_id = numeric(),
+                            datetime_chr_et = character(),
+                            var = character(),
+                            value = numeric(),
+                            unit = character(),
+                            score = numeric(),
+                            nettype = character(),
+                            vartype = character(),
+                            obtype = character(),
+                            obnum = numeric(),
+                            value_accum = numeric())
 
-# define 
-temp_start_date_sel <- "2016-01-01"
-temp_end_date_sel <- "2016-01-31"
+coop_metadata_raw <- data.frame(location_id = numeric(),
+                                network_type = character(),
+                                location_name = character(),
+                                city = character(),
+                                county = character(),
+                                state = character(),
+                                latitude_degrees_north = numeric(),
+                                longitude_degrees_east = numeric(),
+                                elevation_feet = character(),
+                                supporting_agency_for_location = character(),
+                                start_date_chr = character(),
+                                end_date_chr = character(),
+                                obtypes_available = character())
 
-# define url parts that stay the same
-base_url <- "https://climate.ncsu.edu/api/beta/data.php?"
-url_var <- "var=precip1m&" # precipitation at 1 m above Earth's surface
-url_loc <- paste0("loc=type=", ncsco_api_datasets_sel, "&")
-url_state <- "state=NC&"
-url_int <- "int=1 day&" # data interval (1 day = daily)
-url_start_date <- paste0("start=", temp_start_date_sel, "&")
-url_end_date <- paste0("end=", temp_end_date_sel, "&")
-url_output <- "output=csv&" # csv output
-url_key <- paste0("hash=", NCSOC_API_KEY)
-# additional api arguments are here: https://climate.ncsu.edu/api/beta/help
+# record start time
+start_time = now()
 
-# put all together to get query url
-query_url <- paste0(base_url, url_var, url_loc, url_state, url_int, url_start_date, url_end_date, url_output, url_key)
-# query_url <- "https://climate.ncsu.edu/api/beta/data.php?var=precip1m&loc=type=COOP;state=NC&type=meta&int=1 day&start=2016-01-01&end=2017-01-31&output=csv&hash=cf71ae662d7477da3c53da8a1b6d87e49406a28b"
-# query_url <- "https://climate.ncsu.edu/api/beta/data.php?var=precip1m&loc=type=COOP;state=NC&int=1 day&start=2016-01-01&end=2016-01-31&output=csv&hash=cf71ae662d7477da3c53da8a1b6d87e49406a28b"
+for (i in 1:num_months) {
+  # define date range
+  temp_start_date_sel <- date_step_list$start_date[i]
+  temp_end_date_sel <- date_step_list$end_date[i]
+  
+  # define url parts that stay the same
+  base_url <- "https://climate.ncsu.edu/api/beta/data.php?"
+  url_var <- "var=precip1m&" # precipitation at 1 m above Earth's surface
+  url_loc <- paste0("loc=type=", ncsco_api_datasets_sel, "&")
+  url_state <- "state=NC&"
+  url_int <- "int=1 day&" # data interval (1 day = daily)
+  url_start_date <- paste0("start=", temp_start_date_sel, "&")
+  url_end_date <- paste0("end=", temp_end_date_sel, "&")
+  url_output <- "output=csv&" # csv output
+  url_key <- paste0("hash=", NCSOC_API_KEY)
+  # additional api arguments are here: https://climate.ncsu.edu/api/beta/help
+  
+  # put all together to get query url
+  query_url <- paste0(base_url, url_var, url_loc, url_state, url_int, url_start_date, url_end_date, url_output, url_key)
+  # query_url <- "https://climate.ncsu.edu/api/beta/data.php?var=precip1m&loc=type=COOP;state=NC&type=meta&int=1 day&start=2016-01-01&end=2017-01-31&output=csv&hash=cf71ae662d7477da3c53da8a1b6d87e49406a28b"
+  # query_url <- "https://climate.ncsu.edu/api/beta/data.php?var=precip1m&loc=type=COOP;state=NC&int=1 day&start=2016-01-01&end=2016-01-31&output=csv&hash=cf71ae662d7477da3c53da8a1b6d87e49406a28b"
+  
+  # replace spaces in query url with %20 otherwise will get api error
+  query_url_fix <- URLencode(query_url) # need to replace " " with "%20"
+  
+  # check that url exists
+  # url.exists(query_url_fix)
+  # this is still true even when gives 400 error
+  
+  # grab data from url (without metadata)
+  temp_data_raw <- read_csv(file = query_url_fix, comment = "##")  %>% # this grabs just the data, length(test_data) > 1 then there's data, datetime is ET
+    mutate(datetime_chr_et = as.character(datetime)) %>%
+    select(location_id = location, datetime_chr_et, var:value_accum) # make location id column the same as metadata, use date as character columns for now
+  
+  # use function
+  temp_metadata_raw <- read_ncsco_api_metadata(query_url_fix) %>%
+    mutate(start_date_chr = as.character(start_date),
+           end_date_chr = as.character(end_date)) %>%
+    select(location_id:supporting_agency_for_location, start_date_chr, end_date_chr, obtypes_available) 
 
-# replace spaces in query url with %20 otherwise will get api error
-query_url_fix <- URLencode(query_url) # need to replace " " with "%20"
+  # append data
+  coop_data_raw <- bind_rows(temp_data_raw, coop_data_raw)
+  coop_metadata_raw <- bind_rows(temp_metadata_raw, coop_metadata_raw)
+  
+  # print entry status
+  print(paste0("appended month ", i))
+  
+}
 
-# check that url exists
-# url.exists(query_url_fix)
-# this is still true even when gives 400 error
+# record end time
+end_time = now()
 
-# grab data from url (without metadata)
-temp_data_raw <- read_csv(file = query_url_fix, comment = "##") # this grabs just the data
-# length(test_data) > 1 then there's actual data there
-# datetime is ET
+# time to run loop
+end_time - start_time
 
-# use function
-temp_metadata_raw <- read_ncsco_api_metadata(query_url_fix)
-
-
-
-# i know this link works but getting error
-# Error in open.connection(con, "rb") : HTTP error 400.
-# try this? https://stackoverflow.com/questions/60635821/error-in-open-connectioncon-rb-http-error-400-with-r-studio
-# try this: https://www.earthdatascience.org/courses/earth-analytics/get-data-using-apis/namespaces-in-r/
 
 
