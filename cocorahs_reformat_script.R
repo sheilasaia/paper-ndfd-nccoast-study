964529d1dba4b61f11d5292b2295648b484d640c
sheila <sms493@cornell.edu>
2020-09-17 12:53:24 -0400

    cleaned up cocorahs data to merge with sco api data

--- cocorahs_reformat_script.R
+++ cocorahs_reformat_script.R
@@ -1,22 +1,48 @@
 # cocorahs data reformatting script
 
-# ---- 1. load libraries and set paths ----
+# ---- 1. load libraries ----
 library(tidyverse)
 library(sf)
 
-# paths
+
+# ---- 2. define paths ----
+
+# tabular data paths
 tabular_data_raw_path <- "/Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/data/tabular/cocorahs_raw/"
 
+# output path
+tabular_data_output_path <- "/Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/data/tabular/sheila_generated/hist_precip_data/"
 
-# ---- 2. read in data ----
+
+# ---- 3. read in data ----
 # cocorahs data
 cocorahs_data_raw <- read_csv(paste0(tabular_data_raw_path, "cocorahs_data_raw.csv"))
 
 
-# ----- 3. pull out unique sites ----
-# make list of unique station info
-stations_data_tabular <- cocorahs_data_raw %>%
-  distinct(station_id, station_name, lat, long)
+# ---- 4. separate out metadata ----
+cocorahs_metadata <- cocorahs_data_raw %>%
+  # change column names so they are compatible with NC SCO API outputs
+  select(location_id = station_id, location_name = station_name, latitude_degrees_north = lat, longitude_degrees_east = long) %>%
+  distinct_all() %>%
+  # add columns so this is compatible with NC SCO API outputs
+  mutate(network_type = "COCORAHS",
+         city = NA, 
+         county = NA, 
+         state = NA, 
+         elevation_ft_chr = NA, 
+         supporting_agency_for_location = "COCORAHS",
+         start_date_chr = NA,
+         end_date_chr = NA,
+         obtypes_available = NA) %>%
+  select(location_id, network_type, location_name,city:state, latitude_degrees_north, longitude_degrees_east, elevation_ft_chr:obtypes_available)
 
-# convert list to shp file
+
+# ---- 5. separate out data ----
+cocorahs_data <- cocorahs_data_raw %>%
+  select(location_id = station_id, datetime_chr_et = datetime_est, precip_in)
+
+
+# ---- 6. export data ----
+write_csv(x = cocorahs_data, path = paste0(tabular_data_output_path, "cocorahs_data.csv"))
+write_csv(x = cocorahs_data, path = paste0(tabular_data_output_path, "cocorahs_metadata.csv"))
 
