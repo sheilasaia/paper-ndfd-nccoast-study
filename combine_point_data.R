5bdd3374de7d1b21c09bae340150a2c44ce5e51a
sheila <sms493@cornell.edu>
2020-09-17 12:54:12 -0400

    initial commit

---
+++ combine_point_data.R
@@ -0,0 +1,19 @@
+
+# ---- script header ----
+# script name: combine_point_data.R
+# purpose of script: combine all historic point observations of precip
+# author: sheila saia
+# date created: 20200917
+# email: ssaia@ncsu.edu
+
+
+# ---- notes ----
+# notes:
+ 
+
+# ---- to do ----
+# to do list
+
+
+# ----
+
---
+++ compare_data_script.R
@@ -0,0 +1,23 @@
+
+# ---- script header ----
+# script name: compare_data_script.R
+# purpose of script: compare point and grid data
+# author: sheila
+# date created: 20200917
+# email: ssaia@ncsu.edu
+
+
+# ---- notes ----
+# notes:
+ 
+
+# ---- to do ----
+# to do list
+
+
+# ---- 1. load libraries and set paths ----
+library(tidyverse)
+library(sf)
+library(raster)
+
+
