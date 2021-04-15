d9a5d72c46e09a0675c901c3b58f9659b6e43825
sheilasaia <sms493@cornell.edu>
2021-04-15 16:37:21 -0400

    reformatted to run all cmus

--- roc_wrangle_data_script.R
+++ roc_wrangle_data_script.R
@@ -101,7 +101,8 @@ cmu_bounds_lease_count_join_tabular <- cmu_bounds_lease_count_join %>%
 
 # ---- 5. select cmu's for analysis ----
 # cmu list
-cmu_sel_list <- c("U092", "U096", "U144")
+# cmu_sel_list <- c("U092", "U096", "U144") # to start
+cmu_sel_list <- unique(cmu_bounds_albers$cmu_name) # select all
 
 # select cmus
 cmu_bounds_roc_sel <- cmu_bounds_albers %>%
@@ -112,7 +113,7 @@ cmu_bounds_5kmbuf_roc <- cmu_bounds_roc_sel %>%
   st_buffer(dist = 5000) # dist = 5000 means 5km
 
 # plot selected cmu's with buffers
-# pdf(file = here::here("figures", "cmu_selection_with_buffer.pdf"), width = 10, height = 10)
+# pdf(file = here::here("figures", "cmu_selection_with_buffer_fullanalaysis.pdf"), width = 10, height = 10)
 # ggplot() +
 #   geom_sf(data = cmu_bounds_5kmbuf_roc, color = "blue", alpha = 0.75) +
 #   geom_sf(data = cmu_bounds_lease_count_join) + 
@@ -124,17 +125,31 @@ cmu_bounds_5kmbuf_roc <- cmu_bounds_roc_sel %>%
 # ---- 6. find precip data that overlaps ----
 hist_precip_metadata_albers_sel <- hist_precip_metadata_albers %>%
   st_intersection(cmu_bounds_5kmbuf_roc) %>%
-  filter(perc_compl >= 90) # filter out ones that are 90% complete or more
+  dplyr::filter(perc_compl >= 90) # filter out ones that are 90% complete or more
 # without the buffer there are no rain gages within the cmus
 
 # plot cmus and observations
-# pdf(file = here::here("figures", "cmu_selection_with_obs.pdf"), width = 10, height = 10)
+# pdf(file = here::here("figures", "cmu_selection_with_obs_fullanalysis.pdf"), width = 10, height = 10)
 # ggplot() +
 #   geom_sf(data = cmu_bounds_5kmbuf_roc, color = "blue", alpha = 0.75) +
 #   geom_sf(data = cmu_bounds_roc_sel, fill = "blue") +
 #   geom_sf(data = hist_precip_metadata_albers_sel, color = "black") +
 #   theme_classic()
 # dev.off()
+
+# number of unique stations
+length(unique(hist_precip_metadata_albers_sel$loc_id))
+# 37
+ 
+# number of unique cmus
+length(unique(hist_precip_metadata_albers_sel$cmu_name))
+# 91
+
+# total number of cmus
+length(cmu_sel_list)
+# 149
+
+# so we have 91/149 (60%) cmu's represented based on available observed rainfall data
 
 
 # ---- 7. loop through ndfd data to get forecast result ----
@@ -145,14 +160,18 @@ file_list <- list.files(path = ndfd_sco_spatial_data_input_path)
 valid_period_list <- c(24, 48, 72)
 
 # number of observations
-num_obs_stations <- dim(hist_precip_metadata_albers_sel)[1]
+# num_obs_stations <- dim(hist_precip_metadata_albers_sel)[1]
+obs_stations_list <- unique(hist_precip_metadata_albers_sel$loc_id)
+num_obs_stations <- length(obs_stations_list)
 
 # make empty data frame
 ndfd_data_sel <- data.frame(loc_id = as.character(),
                             cmu_name = as.character(),
                             date = as.character(),
                             valid_period_hrs = as.numeric(),
+                            loc_pop_perc = as.numeric(),
                             loc_qpf_in = as.numeric(),
+                            cmu_pop_perc = as.numeric(),
                             cmu_qpf_in = as.numeric())
 # loc_qpf_in is based on the ndfd grid cell that the gage is in
 # cmu_qpf_in is based on the area weighted avg footprint of the cmu associated with the gage (shellcast)
@@ -161,7 +180,7 @@ ndfd_data_sel <- data.frame(loc_id = as.character(),
 start_time <- now()
 
 # for loop (i = day with data, j = valid period, k = observation)
-for (i in 288:dim(data_available)[1]) {
+for (i in 1:2) {#dim(data_available)[1]) {
   # check if data is available
   status <- data_available$status[i]
   
@@ -180,40 +199,62 @@ for (i in 288:dim(data_available)[1]) {
     temp_files <- file_list[grep(pattern = temp_file_pattern, x = file_list)]
     
     # save ndfd file names
-    temp_qpf_file_name <- sort(temp_files[grep(pattern = "qpf", x = temp_files)])
+    temp_pop_file_name <- sort(temp_files[grep(pattern = "pop12", x = temp_files)]) # probability of precipitation (pop)
+    temp_qpf_file_name <- sort(temp_files[grep(pattern = "qpf", x = temp_files)]) # quantitative precipitation forecast (qpf)
     
     # step through each period
     for (j in 1:length(valid_period_list)) {
       # valid period
       temp_valid_period <- valid_period_list[j]
       
+      # print when starts valid period
+      print(paste0("started ", temp_valid_period, " hr valid period"))
+      
       # find valid period files
+      temp_prd_pop_file_name <- temp_pop_file_name[grep(pattern = paste0(as.character(temp_valid_period), "hr"), x = temp_pop_file_name)]
       temp_prd_qpf_file_name <- temp_qpf_file_name[grep(pattern = paste0(as.character(temp_valid_period), "hr"), x = temp_qpf_file_name)]
       
       # read in ndfd raster data
+      temp_pop_raster <- raster::raster(paste0(ndfd_sco_spatial_data_input_path, temp_prd_pop_file_name))
       temp_qpf_raster <- raster::raster(paste0(ndfd_sco_spatial_data_input_path, temp_prd_qpf_file_name))
       
       # save raster resolution
+      temp_pop_raster_res <- raster::res(temp_pop_raster)
       temp_qpf_raster_res <- raster::res(temp_qpf_raster)
       
-      # step through each obs (with unique loc_id and cmu_name)
-      for (k in 1:num_obs_stations) {
-        # save location name
-        temp_loc_id <- as.character(hist_precip_metadata_albers_sel$loc_id[k])
-        
+      # location-based calcs
+      # get location
+      temp_loc_coord <- hist_precip_metadata_albers_sel %>%
+        dplyr::select(geometry)
+      
+      # get value of gridcell that overlaps with each unique location
+      temp_loc_pop_list <- raster::extract(temp_pop_raster, temp_loc_coord, weights = FALSE)
+      temp_loc_qpf_list <- round(raster::extract(temp_qpf_raster, temp_loc_coord, weights = FALSE), 4)
+      
+      # location-based calcs df
+      temp_loc_result_df <- data.frame(loc_id = hist_precip_metadata_albers_sel$loc_id,
+                                       temp_loc_pop_result = temp_loc_pop_list,
+                                       temp_loc_qpf_result = temp_loc_qpf_list) %>%
+        dplyr::distinct()
+      # there are redundant stations in hist_precip_metadata_albers_sel
+      
+      # cmu-based calcs (like shellcast)
+      # get unique cmu list
+      unique_cmu_list <- unique(hist_precip_metadata_albers_sel$cmu_name)
+      num_unique_cmu <- length(unique(hist_precip_metadata_albers_sel$cmu_name))
+      
+      # cmu-based calcs df
+      # start with one that's empty
+      temp_cmu_result_df <- data.frame(cmu_name = as.character(),
+                                       temp_cmu_pop_result = as.numeric(),
+                                       temp_cmu_qpf_result = as.numeric())
+      
+      # run for each unique cmu and append to temp_cmu_result_df
+      # do this because there are redundant cmus in hist_precip_metadata_albers_sel
+      for (k in 1:num_unique_cmu) {
         # save cmu name
-        temp_cmu_name <- as.character(hist_precip_metadata_albers_sel$cmu_name[k])
+        temp_cmu_name <- as.character(unique_cmu_list[k])
         
-        # location-based calcs
-        # get location location
-        temp_loc_coord <- hist_precip_metadata_albers_sel %>%
-          dplyr::filter(loc_id == temp_loc_id) %>%
-          dplyr::select(geometry)
-        
-        # get value of gridcell that overlaps with location
-        temp_loc_qpf_result <- round(raster::extract(temp_qpf_raster, temp_loc_coord, weights = FALSE)[[1]], 4)
-        
-        # cmu-based calcs (like shellcast)
         # get cmu bounds vector
         temp_cmu_bounds <- cmu_bounds_roc_sel %>%
           dplyr::filter(cmu_name == temp_cmu_name) %>%
@@ -223,32 +264,50 @@ for (i in 288:dim(data_available)[1]) {
         temp_cmu_area <- as.numeric(st_area(temp_cmu_bounds)) # in m^2
         
         # get value and weight of all gridcells that overlap the cmu
+        temp_pop_cmu_raster_perc_cover_df <- data.frame(raster::extract(temp_pop_raster, temp_cmu_bounds, weights = TRUE)[[1]])
         temp_qpf_cmu_raster_perc_cover_df <- data.frame(raster::extract(temp_qpf_raster, temp_cmu_bounds, weights = TRUE)[[1]])
         
         # calculate area weighted avg value for the pts
+        temp_pop_area_weighted_df <- temp_pop_cmu_raster_perc_cover_df %>%
+          dplyr::mutate(area_weighted_avg = value * weight)
         temp_qpf_area_weighted_df <- temp_qpf_cmu_raster_perc_cover_df %>%
           dplyr::mutate(area_weighted_avg = value * weight)
         
         # sum weighted values to get cmu result
-        temp_cmu_qpf_result <- round(sum(temp_qpf_area_weighted_df$area_weighted_avg), 4)
-        
         # save data
-        temp_ndfd_data_sel <- data.frame(loc_id = temp_loc_id,
-                                         cmu_name = temp_cmu_name,
-                                         date = temp_date,
-                                         valid_period_hrs = temp_valid_period,
-                                         loc_qpf_in = temp_loc_qpf_result,
-                                         cmu_qpf_in = temp_cmu_qpf_result)
+        temp_cmu_result_df_part <- data.frame(cmu_name = temp_cmu_name,
+                                              temp_cmu_pop_result = round(sum(temp_pop_area_weighted_df$area_weighted_avg), 4),
+                                              temp_cmu_qpf_result = round(sum(temp_qpf_area_weighted_df$area_weighted_avg), 4))
         
         # bind results
-        ndfd_data_sel <-  rbind(ndfd_data_sel, temp_ndfd_data_sel)
+        temp_cmu_result_df <-  rbind(temp_cmu_result_df, temp_cmu_result_df_part)
         
         # print when finished with point
-        # print(paste0("finished row: ", k))
+        # print(paste0("finished cmu: ", k, " of ", num_unique_cmu))
       }
       
+      # join location- and cmu-based results together
+      temp_loc_cmu_results_df_join <- hist_precip_metadata_albers_sel %>%
+        st_drop_geometry() %>%
+        dplyr::select(loc_id, cmu_name) %>%
+        dplyr::left_join(temp_loc_result_df, by = "loc_id") %>%
+        dplyr::left_join(temp_cmu_result_df, by = "cmu_name")
+      
+      # save data
+      temp_ndfd_data_sel <- data.frame(loc_id = temp_loc_cmu_results_df_join$loc_id,
+                                       cmu_name = temp_loc_cmu_results_df_join$cmu_name,
+                                       date = temp_date,
+                                       valid_period_hrs = temp_valid_period,
+                                       loc_pop_perc = temp_loc_cmu_results_df_join$temp_loc_pop_result,
+                                       loc_qpf_in = temp_loc_cmu_results_df_join$temp_loc_qpf_result,
+                                       cmu_pop_perc = temp_loc_cmu_results_df_join$temp_cmu_qpf_result,
+                                       cmu_qpf_in = temp_loc_cmu_results_df_join$temp_cmu_qpf_result)
+      
+      # bind results
+      ndfd_data_sel <-  rbind(ndfd_data_sel, temp_ndfd_data_sel)
+      
       # print when done with period
-      print(paste0(temp_valid_period, " hr period appended for ", temp_date))
+      print(paste0(temp_valid_period, " hr valid period completled for ", temp_date))
     }
     
     # print when finished with day
@@ -266,7 +325,7 @@ stop_time <- now()
 
 # time to run loop
 stop_time - start_time
-# 1.5 hrs for 3 cmus
+# ~3 min for 1 day x 731 days = 2193 min / 60 = 36 hrs
 
 # ---- 8. check how similar loc and cmu approaches are ----
 # plot loc qpf vs cmu qpf (do ROC for both?)
@@ -280,14 +339,18 @@ ggplot(data = ndfd_data_sel) +
 
 # ---- 9. export data ----
 # export spatial data
-st_write(cmu_bounds_roc_sel, paste0(data_path, "spatial/sheila_generated/cmu_bounds/cmu_bounds_roc_sel.shp"), delete_layer = TRUE)
-st_write(cmu_bounds_5kmbuf_roc, paste0(data_path, "spatial/sheila_generated/cmu_bounds/cmu_bounds_5kmbuf_roc.shp"), delete_layer = TRUE)
+# st_write(cmu_bounds_roc_sel, paste0(data_path, "spatial/sheila_generated/cmu_bounds/cmu_bounds_roc_sel.shp"), delete_layer = TRUE)
+# st_write(cmu_bounds_5kmbuf_roc, paste0(data_path, "spatial/sheila_generated/cmu_bounds/cmu_bounds_5kmbuf_roc.shp"), delete_layer = TRUE)
+st_write(cmu_bounds_roc_sel, paste0(data_path, "spatial/sheila_generated/cmu_bounds/cmu_bounds_roc_full.shp"), delete_layer = TRUE)
+st_write(cmu_bounds_5kmbuf_roc, paste0(data_path, "spatial/sheila_generated/cmu_bounds/cmu_bounds_5kmbuf_roc_full.shp"), delete_layer = TRUE)
 
 # export precip metadata that overlaps and is 90% complete
-st_write(hist_precip_metadata_albers_sel, paste0(data_path, "spatial/sheila_generated/hist_precip_data/hist_precip_metadata_albers_sel.shp"), delete_layer = TRUE)
-         
+# st_write(hist_precip_metadata_albers_sel, paste0(data_path, "spatial/sheila_generated/hist_precip_data/hist_precip_metadata_albers_sel.shp"), delete_layer = TRUE)
+st_write(hist_precip_metadata_albers_sel, paste0(data_path, "spatial/sheila_generated/hist_precip_data/hist_precip_metadata_albers_full.shp"), delete_layer = TRUE)
+
 # export ndfd data
-write_csv(ndfd_data_sel, paste0(data_path, "tabular/sheila_generated/ndfd_sco_hist/ndfd_data_sel.csv"))
+# write_csv(ndfd_data_sel, paste0(data_path, "tabular/sheila_generated/ndfd_sco_hist/ndfd_data_sel.csv"))
+write_csv(ndfd_data_sel, paste0(data_path, "tabular/sheila_generated/ndfd_sco_hist/ndfd_data_full.csv"))
 
 
 
