34759b40ee0a918aa64251c9cd1da2ddf24ab478
ssaia <ssaia@ncsu.edu>
2021-07-22 15:29:26 -0400

    initial commit

---
+++ 10_check_ndfd_data_script.R
@@ -0,0 +1,14 @@
+# ---- script header ----
+# script name: 10_check_ndfd_data_script.R
+# purpose of script: check (including evaluating using model metrics) ndfd data against obs data
+# author:
+# date created:
+# email:
+
+
+# ---- notes ----
+# notes:
+
+
+# ---- to do ----
+# to do list: 
